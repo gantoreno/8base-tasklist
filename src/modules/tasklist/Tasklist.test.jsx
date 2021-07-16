2cdca6b95f3867c7be75d0802ef7ba01750e0238
Gabriel Moreno <gantoreno@gmail.com>
2021-07-15 22:23:09 -0400

    Fix import on `Tasklist.test.jsx`

--- src/modules/tasklist/Tasklist.test.jsx
+++ src/modules/tasklist/Tasklist.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 
 import AppProvider from '../../shared/8base/AppProvider';
-import Tasklist from './TaskList';
+import Tasklist from './Tasklist';
 
 describe('Tasklist', () => {
   it('renders without crashing', () => {
